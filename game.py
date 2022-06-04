6be4c0b0990eca0bd28c3349ba9cd7a78a53d7c9
hiryrwim <104685005+hiryrwim@users.noreply.github.com>
2022-06-04 16:40:49 +0300

    Add files via upload

--- game.py
+++ game.py
@@ -2,13 +2,12 @@ from pygame import *
 
 
 class GamePlayer(sprite.Sprite):
-    def __init__(self, img, width, x, y, step):
+    def __init__(self, img, width, height, x, y, step):
         super().__init__()
         self.image = transform.scale(
-            image.load(img,
+            image.load(img),
             (width, height)
             )
-        )
         self.height = height
         self.rect = self.image.get_rect()
         self.rect.x = x
@@ -23,7 +22,7 @@ class LeftPlayer(GamePlayer):
         keys = key.get_pressed()
         if keys[K_w] and self.rect.y > 0:
             self.rect.y -= self.step
-        elif keys[k_s] and self.rect.y < 500 - self.height:
+        elif keys[K_s] and self.rect.y < 500 - self.height:
             self.rect.y += self.step
             
 class RightPlayer(GamePlayer):
@@ -37,17 +36,20 @@ class RightPlayer(GamePlayer):
 window = display.set_mode((700, 500))
 background = transform.scale(
     image.load('ping pong.jpg'),
-    (500, 500)
+    (700, 500)
 )
 fps = 60
-player_1 = LeftPlayer('bobr.jpg', 40, 50, 50)
 clock = time.Clock()
+game = True
+player_1 = LeftPlayer('bobr.jpg', 40, 80, 40, 210, 10)
 game = True
 while game:
     for e in event.get():
         if e.type == QUIT:
             game = False
         window.blit(background, (0, 0))
+        player_1.update()
+        player_1.reset()
         display.update()
         clock.tick(fps)
-        player_1.update()
+        
